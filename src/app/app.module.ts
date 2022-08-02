9bfb3c12394fae1dca7e3aeac828ceed192c87f2
David2813 <quinterosotodavidjavier@gmail.com>
2022-08-02 16:40:14 -0500

    Primer commit con la creacion de las estructuras

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { OfertasComponetComponent } from './pages/ofertas-componet/ofertas-componet.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    OfertasComponetComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/pages/ofertas-componet/ofertas-componet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ofertas-componet',
+  templateUrl: './ofertas-componet.component.html',
+  styleUrls: ['./ofertas-componet.component.css']
+})
+export class OfertasComponetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
